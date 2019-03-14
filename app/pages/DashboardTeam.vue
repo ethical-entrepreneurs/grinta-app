701c982c42bf45c1a2edfa01b84d22f2ac404df8
Vincent Battez <vincent.battez.pro@gmail.com>
2019-03-14 16:58:59 +0100

    feat: DashboardTeam

---
+++ app/pages/DashboardTeam.vue
@@ -0,0 +1,61 @@
+<template>
+  <Page class="page page--dashboard-team">
+    <ActionBar title="DktTeam Dashboard" class="action-bar" />
+    <FlexboxLayout flexDirection="column">
+      <StackLayout>
+
+        <Button text="Create un Grinta Challenge" class="btn btn--primary" @tap="CreateGrinta" />
+        <Label text="Sportifs:" textWrap="true" class="title" />
+        
+        <ListView for="user in team.users">
+          <v-template>
+            <StackLayout>
+              <Label :text="user.name" />
+            </StackLayout>
+          </v-template>
+        </ListView>
+
+      </StackLayout>
+    </FlexboxLayout>
+  </Page>
+</template>
+
+<script>
+  import GrintaTuto from "./GrintaTuto";
+
+  export default {
+    data() {
+      return {
+        team: {
+          users: [
+            {
+              name: 'Eddy Malou'
+            },
+            {
+              name: 'Sylvain Durif'
+            },
+          ],
+        },
+      }
+    },
+    methods: {
+      CreateGrinta() {
+        console.log('Create()')
+         this.$navigateTo(GrintaTuto)
+      },
+    },
+  }
+</script>
+
+<style scoped>
+  .action-bar {
+    padding: 0;
+    margin-bottom: 20;
+  }
+
+  .title {
+    font-size: 18;
+    margin-top: 20;
+    margin-bottom: 10;
+  }
+</style>
