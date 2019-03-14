7b8f6eb5953695c290a5f20d7cfe7e2213e2b5ae
Vincent Battez <vincent.battez.pro@gmail.com>
2019-03-14 15:15:39 +0100

    feat(functionality): Login Page

---
+++ app/pages/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <Page class="page page--login">
+    <FlexboxLayout flexDirection="column" justifyContent="space-around">
+      <Button text="Se connecter avec Décathlon" class="btn btn--primary" @tap="loginDecathlon" />
+      <Label text="Ou" textWrap="true" horizontalAlignment="center"/>
+      <Button text="Créer un compte" class="btn btn--secondary" @tap="createAccount" />
+    </FlexboxLayout>
+  </Page>
+</template>
+
+<script>
+  import InviteFriend from "./InviteFriend";
+
+  export default {
+    methods: {
+      loginDecathlon() {
+        this.$navigateTo(InviteFriend);
+      },
+      // createAccount() {
+      //   this.$navigateTo(DetailPeople);
+      // },
+    },
+  }
+</script>
+
+<style scoped>
+  /* .home-panel {
+    vertical-align: center;
+    font-size: 20;
+    margin: 15;
+  }
+
+  .description-label {
+    margin-bottom: 15;
+  } */
+</style>
