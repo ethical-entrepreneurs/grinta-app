b5e2b41139722fd2ab4a387e8a897e1b728c26e2
Vincent Battez <vincent.battez.pro@gmail.com>
2019-03-14 15:01:00 +0100

    feat(functionality): Welcome Page

---
+++ app/pages/Welcome.vue
@@ -0,0 +1,25 @@
+<template>
+  <Page class="page page--home">
+    <FlexboxLayout flexDirection="column" justifyContent="space-around">
+      <StackLayout>
+        <Label text="Bienvenue sur" textWrap="true" horizontalAlignment="center"/>
+        <Image src="~/assets/img/logo.png" stretch="aspectFit" />
+      </StackLayout>
+      <StackLayout>
+        <Label text="l'app de challenge sportif entre amis !" textWrap="true" horizontalAlignment="center"/>
+        <Button text="Let's Go !" class="btn btn--primary" @tap="goToNextPage" />
+      </StackLayout>
+    </FlexboxLayout>
+  </Page>
+</template>
+
+<script>
+import Login from "./Login";
+  export default {
+    methods: {
+      goToNextPage(event) {
+        this.$navigateTo(Login);
+      }
+    },
+  }
+</script>
