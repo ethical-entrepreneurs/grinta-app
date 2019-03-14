bc8e1e2922c61a52409e217bbd7bf838635cf436
Vincent Battez <vincent.battez.pro@gmail.com>
2019-03-14 11:29:13 +0100

    🚀 GRIIINTAA (first commit)

---
+++ app/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <Page class="page">
+    <ActionBar title="Home" class="action-bar" />
+    <ScrollView>
+      <StackLayout class="home-panel">
+        <Label textWrap="true" text="Play with NativeScript!" class="h2 description-label" />
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {};
+    },
+  }
+</script>
+
+<style scoped>
+  .home-panel {
+    vertical-align: center;
+    font-size: 20;
+    margin: 15;
+  }
+
+  .description-label {
+    margin-bottom: 15;
+  }
+</style>
