65ccca33b0a52fdb2eccd2ebbdfe093b6b3549da
Vincent Battez <vincent.battez.pro@gmail.com>
2019-03-15 10:51:41 +0100

    style: Homepage design

--- app/pages/Welcome.vue
+++ app/pages/Welcome.vue
@@ -2,12 +2,14 @@
   <Page class="page page--home">
     <FlexboxLayout flexDirection="column" justifyContent="space-around">
       <StackLayout>
-        <Label text="Bienvenue sur" textWrap="true" horizontalAlignment="center"/>
-        <Image src="~/assets/img/logo.png" stretch="aspectFit" />
-      </StackLayout>
-      <StackLayout>
-        <Label text="l'app de challenge sportif entre amis !" textWrap="true" horizontalAlignment="center"/>
-        <Button text="Let's Go !" class="btn btn--primary" @tap="goToNextPage" />
+        <Image src="~/assets/img/logo.png" stretch="aspectFit" class="logo" />
+        <Label
+          text="L’application de challenges sportifs entre amis ! "
+          textWrap="true"
+          horizontalAlignment="center"
+          class="slogan"
+        />
+        <Button text="On y va !" class="btn btn--bg" @tap="goToNextPage" />
       </StackLayout>
     </FlexboxLayout>
   </Page>
@@ -15,11 +17,33 @@
 
 <script>
 import Login from "./Login";
-  export default {
-    methods: {
-      goToNextPage() {
-        this.$navigateTo(Login);
-      }
-    },
+export default {
+  methods: {
+    goToNextPage() {
+      this.$navigateTo(Login);
+    }
   }
+};
 </script>
+
+<style scoped lang="scss">
+.page {
+  &--home {
+    background-image: url("~/assets/img/bg-hp.png");
+    background-size: cover;
+
+    .logo {
+      width: 70%;
+    }
+
+    .slogan {
+      color: white;
+      font-size: 21px;
+      width: 80%;
+      text-align: center;
+      margin-top: 10;
+      margin-bottom: 80;
+    }
+  }
+}
+</style>
